be1d8401932dd87822baea50468e8fffaa9c2f23
abedshadin <abedshadin@gmail.com>
2022-04-17 13:04:11 +0600

    img,desc added on services

--- src/Services/Service/Service.js
+++ src/Services/Service/Service.js
@@ -3,7 +3,7 @@ import { Card, ListGroup } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 
 const Service = ({service}) => {
-    const {pack,click,time,team_member,price} = service;
+    const {pack,click,time,team_member,price,img,desc} = service;
     const navigate = useNavigate();
     const navigateCheck= (pack) =>{
 navigate(`/checkout/${pack}`)
@@ -14,11 +14,13 @@ navigate(`/checkout/${pack}`)
   <div className="card-header h3">
     {pack}
   </div>
-  <ul className="list-group list-group-flush">
+  <ul className="list-group list-group-flcush">
+    <li className="list-group-item"><img src={img} className='w-25' alt="" /> </li>
     <li className="list-group-item bg-success text-light h2">{price} TK</li>
     <li className="list-group-item">Total Clicks: {click}</li>
     <li className="list-group-item">Time: {time} Hours</li>
     <li className="list-group-item">Members: {team_member}</li>
+    <li className="list-group-item"> {desc}</li>
     <li className="list-group-item"><button onClick={()=>navigateCheck(pack)} className='btn btn-success'>Order Now</button></li>
 
   </ul>
