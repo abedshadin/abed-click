eaea6ae4247ba180e050d0949baf062f030d9374
abedshadin <abedshadin@gmail.com>
2022-04-18 11:28:03 +0600

    checkout edited

--- src/Checkout/Checkout.js
+++ src/Checkout/Checkout.js
@@ -1,19 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { toast, ToastContainer } from 'react-toastify';
+
 
 
 const Checkout = () => {
     const {pack} = useParams();
-    const notify = () => toast("Wow so easy !");
+ 
  
     return (
-        <div className='container text-center mt-5'>
+        <div className='container text-center mt-5 minRegLogin'>
         <h1> Package : {pack}</h1>
-        <div>
-        <button onClick={notify}>Notify !</button>
-        <ToastContainer />
-      </div>
+      
         </div>
     );
 };
