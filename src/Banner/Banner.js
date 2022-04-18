7cead9cc9b5365976d8293c8309522e3154d6d67
abedshadin <abedshadin@gmail.com>
2022-04-18 10:39:38 +0600

    banner pic change

--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -9,35 +9,26 @@ const Banner = () => {
   <Carousel.Item interval={1000}>
     <img
       className="d-block w-100"
-      src="https://cdn.pixabay.com/photo/2021/09/12/07/58/banner-6617553__340.jpg"
+      src="https://i.ibb.co/8KBbKXS/Green-Black-Rustic-Flower-Illustration-Modern-Happy-Wedding-Banners.png"
       alt="First slide"
     />
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
+   
   </Carousel.Item>
   <Carousel.Item interval={500}>
     <img
       className="d-block w-100"
-      src="https://cdn.pixabay.com/photo/2021/09/12/07/58/banner-6617553__340.jpg"
+      src="https://i.ibb.co/6bTzJKs/Anniversary-Banner-Landscape.png"
       alt="Second slide"
     />
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
+  
   </Carousel.Item>
   <Carousel.Item>
     <img
       className="d-block w-100"
-      src="https://cdn.pixabay.com/photo/2021/09/12/07/58/banner-6617553__340.jpg"
+      src="https://i.ibb.co/L5LjbRG/White-Store-Sign-Shape-and-Background-Rustic-Flower-Illustration-Welcome-To-Our-Wedding-Banners.png"
       alt="Third slide"
     />
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
+
   </Carousel.Item>
 </Carousel>
         </div>
